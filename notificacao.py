cecec4e71809664c55b67954802d7c29319c0b94
Rafalhel <rafaelgs04@gmail.com>
2022-09-05 13:27:00 -0300

    contagem recomeça a partir de ultima verificação de 9 digitos presente em naoPalindromo + backup Dados

--- notificacao.py
+++ notificacao.py
@@ -5,6 +5,6 @@ class notificacao:
         self.toast = ToastNotifier()
     def notificar(self, titulo: str, mensagem: str):
         self.toast.show_toast(titulo, mensagem,  duration=20)
-'217468389'
-'883163882'
-'git remote add origin https://github.com/Rafalhel/desafio'
+# '217468389'
+# '883163882'
+# 'git remote add origin https://github.com/Rafalhel/desafio'
