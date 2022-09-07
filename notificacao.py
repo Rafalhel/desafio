7b0cb858fddf728bbb419bac9ad645e259259feb
Rafalhel <rafaelgs04@gmail.com>
2022-09-07 14:33:09 -0300

    backup Dados

--- notificacao.py
+++ notificacao.py
@@ -9,3 +9,10 @@ class notificacao:
 # '883163882'
 # 'git remote add origin https://github.com/Rafalhel/desafio'
 # 628478559
+# '950130411'
+'616692114'
+'753595893'
+'304016642'
+'104962917'
+'659703795'
+'204706746'
